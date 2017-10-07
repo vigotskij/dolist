c6dad48428196bb1bc7b40d3c84b0447031d5ded
vigotskij <mdqulto@gmail.com>
2017-10-07 12:30:43 -0300

    Code reorganization. Seeking for new approach.

--- main.cpp
+++ main.cpp
@@ -3,18 +3,19 @@
 #endif
 #include <stdlib>
 #include <iostream>
+#include "dolist.temp.spec.h"
 
 int main( int argv , char* argc[] ){
 	IList<char*> *list = factoryList<char*>() ;
-
+	/*
 	list->insert( "hola" ) ;
 	std::cout << list->extractByValue( "hola" ) << std::endl ;
 	list->insert( "chau" ) ;
-    list->insert( "gablgabl" ) ;
+	list->insert( "gablgabl" ) ;
 	std::cout << list->indexOf( "chau" ) << std::endl ;
 	std::cout << list->extractFirst() << std::endl ;
 	std::cout << list->extractFirst() << std::endl ;
-
+	*/
 	system("pause") ;
 
 	delete list ;
